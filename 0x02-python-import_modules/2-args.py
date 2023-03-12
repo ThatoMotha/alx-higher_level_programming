4c5f7d0699cb6a0ddd48e183c0fa970416c15929
ThatoMotha <tmafukati@gmail.com>
2023-03-12 06:54:57 -0700

    task2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    if num_args == 0:
+        print("Number of argument(s): 0.")
+        print(".")
+    elif num_args == 1:
+        print("NUmber of argument(s): 1. Argument:")
+        print("1: {}".format(args[0]))
+    else:
+        print("Number of argument(s): {}. Arguments:".format(num_args))
+        for i in range(num_args):
+            print("{}: {}".format(i+1, args[i]))
